c025c8353821596ea3c0496d2c9743cd0c77ec6c
Why? <1223414335@qq.com>
2024-05-24 16:14:13 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = '.';
+const outputFilePath = path.join(rootDir, 'index.json');
+let mergedData = [];
+
+function readJsonFiles(dir) {
+  const files = fs.readdirSync(dir);
+
+  files.forEach(file => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isDirectory()) {
+      readJsonFiles(filePath);
+    } else if (file === 'index.json') {
+      const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+      mergedData.push(data);
+    }
+  });
+}
+
+readJsonFiles(rootDir);
+
+fs.writeFileSync(outputFilePath, JSON.stringify(mergedData, null, 2));
+console.log(`Merged ${mergedData.length} index.json files into ${outputFilePath}`);
